abfaab077589bab0c58300e292e655e3b7b782b6
Ramón García Verjaga <ramongarver2000@gmail.com>
2022-05-11 01:43:50 +0200

    Añadida clase constante para simular el tiempo

---
+++ code/es.ugr.murat/src/es/ugr/murat/constant/TimeConstant.java
@@ -0,0 +1,10 @@
+package es.ugr.murat.constant;
+
+public class TimeConstant {
+
+    public static final int SECOND = 1;
+    public static final int MINUTE = 60 * SECOND;
+    public static final int HOUR = 60 * MINUTE;
+    public static final int DAY = 24 * HOUR;
+
+}
