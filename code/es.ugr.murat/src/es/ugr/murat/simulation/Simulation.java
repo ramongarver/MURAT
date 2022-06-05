5b73cd60960237e4ff01ee1a69b7e0997fdb6d1d
Ramón García Verjaga <ramongarver2000@gmail.com>
2022-06-05 11:46:11 +0200

    Eliminado import sobrante

--- code/es.ugr.murat/src/es/ugr/murat/simulation/Simulation.java
+++ code/es.ugr.murat/src/es/ugr/murat/simulation/Simulation.java
@@ -22,7 +22,6 @@ import es.ugr.murat.model.TrafficLightModel;
 
 import javax.swing.JOptionPane;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.Duration;
